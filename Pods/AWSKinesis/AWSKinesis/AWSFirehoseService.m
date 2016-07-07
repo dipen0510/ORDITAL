2c717b7bb4110c399ec28c9cb6bfa4a0e2c485f7
Dipen Sekhsaria <dipen0510@gmail.com>
2016-07-07 23:54:02 +0530

    - pods update

--- Pods/AWSKinesis/AWSKinesis/AWSFirehoseService.m
+++ Pods/AWSKinesis/AWSKinesis/AWSFirehoseService.m
@@ -27,7 +27,7 @@
 #import "AWSFirehoseResources.h"
 
 static NSString *const AWSInfoFirehose = @"Firehose";
-static NSString *const AWSFirehoseSDKVersion = @"2.4.3";
+static NSString *const AWSFirehoseSDKVersion = @"2.4.5";
 
 @interface AWSFirehoseResponseSerializer : AWSJSONResponseSerializer
 
